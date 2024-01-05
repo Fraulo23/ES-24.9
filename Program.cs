3287010b158bd126fc2da2069661d556104b3863
Fraulo23 <fraulinifederico@gmail.com>
2024-01-05 12:14:14 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_24._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(2, 2);
+            Vector v2= new Vector(3, 3);
+            Vector v3 = Vector.parse("4;4");
+            Console.WriteLine("La somma tra le componenti del vettore 1 e il vettore 2 sono: {0}:{1}", v1.Somma(v2).X, v1.Somma(v2).Y);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Vector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ES_24._9
+{
+    internal class Vector
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public Vector(float x, float y)
+        {
+            X = x;
+            Y = y;
+        }
+        public Vector Somma(Vector v2)
+        {
+            float X=this.X+v2.X;
+            float Y=this.Y+v2.Y;
+            return new Vector(X, Y);
+        }
+        public static Vector parse(string componenti)
+        {
+            string[] Componente = componenti.Split(';');
+            float X = float.Parse(Componente[0]);
+            float Y = float.Parse(Componente[1]);
+            return new Vector(X, Y);
+        }
+    }
+}
